83d5572c1090500ffb43edb9a1f48c0e6bd92bff
MichalJakubHoppe <96836371+MichalJakubHoppe@users.noreply.github.com>
2022-06-13 19:37:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+#Mini Lotto JS project
+
+app generates random numbers for lotto
+
+live preview:
+
+includes button and nice visuals for the end user
+
+used: for loop, Math.floor(),
+Math.random(), map(), DOM.
---
+++ index.js
@@ -0,0 +1,20 @@
+function getLottoNumbers(){
+    const winningNums = []
+    for (let i = 0; i < 6; i++){
+        winningNums.push(Math.round(Math.random()*100))
+    }
+    return winningNums
+}
+
+function getWinningNumbersHtml(){
+    return getLottoNumbers().map(function(num){
+        return `<div class="number">${num}</div>`
+    }).join('')
+}
+
+// document.getElementById('winning-numbers').innerHTML = getWinningNumbersHtml()
+
+const startLotto = document.getElementById("btn")
+startLotto.addEventListener("click",function(){
+    document.getElementById('winning-numbers').innerHTML = getWinningNumbersHtml()
+})
